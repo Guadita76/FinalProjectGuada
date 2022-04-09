e7fb4e102c7adc0e11937bcb8c52cc03f97deea0
Guadalupe <lupelima67@gmail.com>
2022-04-08 21:23:40 -0300

    first commit

---
+++ projectguadafit/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from projectguadafit.users.models import User
+from projectguadafit.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def media_storage(settings, tmpdir):
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user() -> User:
+    return UserFactory()
